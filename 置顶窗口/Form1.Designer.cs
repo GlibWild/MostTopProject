931211f62ea9746c797e39ff8c1721f4d1434589
xiahongqiang <1075012871@qq.com>
2018-10-23 17:12:10 +0800

    first commit

---
+++ 置顶窗口/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace 置顶窗口
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tGBK = new System.Windows.Forms.ToolStripMenuItem();
+            this.tANSI = new System.Windows.Forms.ToolStripMenuItem();
+            this.tUTF8 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 315);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 315);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 293);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(481, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.Visible = false;
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tGBK,
+            this.tANSI,
+            this.tUTF8});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 20);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripDropDownButton1_DropDownItemClicked);
+            // 
+            // tGBK
+            // 
+            this.tGBK.Name = "tGBK";
+            this.tGBK.Size = new System.Drawing.Size(105, 22);
+            this.tGBK.Text = "GBK";
+            // 
+            // tANSI
+            // 
+            this.tANSI.Checked = true;
+            this.tANSI.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tANSI.Name = "tANSI";
+            this.tANSI.Size = new System.Drawing.Size(105, 22);
+            this.tANSI.Text = "ANSI";
+            // 
+            // tUTF8
+            // 
+            this.tUTF8.Name = "tUTF8";
+            this.tUTF8.Size = new System.Drawing.Size(105, 22);
+            this.tUTF8.Text = "UTF8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 315);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem tGBK;
+        private System.Windows.Forms.ToolStripMenuItem tANSI;
+        private System.Windows.Forms.ToolStripMenuItem tUTF8;
+    }
+}
+
---
+++ 置顶窗口/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 置顶窗口
+{
+    public partial class Form1 : Form
+    {
+        string fileName = string.Empty;
+        string[] PicExtensions = { ".gif", ".jpg", ".jpeg", ".png", ".bmp" };
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+        void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            int width = this.Width;
+            int height = this.Height;
+            Rectangle rect = System.Windows.Forms.SystemInformation.VirtualScreen;
+            this.Location = new Point(rect.Width - width, 0);
+            this.AllowDrop = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.TopMost = true;
+        }
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            TranslateToFile(e);
+        }
+
+        private object TranslateToFile(DragEventArgs e)
+        {
+            object obj = null;
+            try
+            {
+                if (obj == null)
+                {
+                    string format = DataFormats.Text;
+                    obj = e.Data.GetData(format, false);
+                    if (obj != null)
+                    {
+                        this.pictureBox1.Visible = false;
+                        this.richTextBox1.Visible = true;
+                        this.statusStrip1.Visible = true;
+                        this.richTextBox1.Text = (String)obj;
+                        TextWriter writer = new StreamWriter(Path.Combine(Application.StartupPath, "~tmp.txt"),false);
+                        writer.Write((String)obj);
+                        writer.Close();
+                        FileInfo file = new FileInfo(Path.Combine(Application.StartupPath, "~tmp.txt"));
+                        file.Attributes = FileAttributes.Hidden;
+                        fileName = file.FullName;
+                    }
+                }
+                if (obj == null)
+                {
+                    string format = DataFormats.Bitmap;
+                    obj = e.Data.GetData(format, false);
+                    if (obj != null)
+                    {
+                        this.pictureBox1.Visible = true;
+                        this.richTextBox1.Visible = false;
+                        this.statusStrip1.Visible = false;
+                        this.pictureBox1.Image = (Bitmap)obj;
+                    }
+                }
+                if (obj == null)
+                {
+                    string format = DataFormats.FileDrop;
+                    obj = e.Data.GetData(format, false);
+                    if (obj != null)
+                    {
+                        //获取第一个文件名
+                        fileName = (obj as String[])[0];
+                        try
+                        {
+                            if (PicExtensions.Contains(Path.GetExtension(fileName).ToLower()))
+                            {
+                                this.pictureBox1.Visible = true;
+                                this.richTextBox1.Visible = false;
+                                this.statusStrip1.Visible = false;
+                                this.pictureBox1.ImageLocation = fileName;
+                            }
+                            else
+                            {
+                                this.pictureBox1.Visible = false;
+                                this.richTextBox1.Visible = true;
+                                this.statusStrip1.Visible = true;
+                                this.richTextBox1.Clear();
+                                foreach (var item in File.ReadLines(fileName, Encoding.Default))
+                                {
+                                    string tmp = item.Replace("\0", "");
+                                    this.richTextBox1.AppendText(tmp);
+                                    this.richTextBox1.AppendText(Environment.NewLine);
+                                }
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            this.pictureBox1.Visible = false;
+                            this.richTextBox1.Visible = false;
+                            this.statusStrip1.Visible = false;
+                            MessageBox.Show("文件格式不对");
+                        }
+                    }
+                }
+            }
+            catch { }
+            return obj;
+        }
+
+        private void toolStripDropDownButton1_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            this.richTextBox1.Clear();
+            if (e.ClickedItem.Name == "tUTF8")
+            {
+                tUTF8.Checked = true;
+                tANSI.Checked = false;
+                tGBK.Checked = false;
+                foreach (var item in File.ReadLines(fileName, Encoding.UTF8))
+                {
+                    string tmp = item.Replace("\0", "");
+                    this.richTextBox1.AppendText(tmp);
+                    this.richTextBox1.AppendText(Environment.NewLine);
+                }
+            }
+            else if (e.ClickedItem.Name == "tANSI")
+            {
+                tUTF8.Checked = false;
+                tANSI.Checked = true;
+                tGBK.Checked = false;
+                foreach (var item in File.ReadLines(fileName, Encoding.Default))
+                {
+                    string tmp = item.Replace("\0", "");
+                    this.richTextBox1.AppendText(tmp);
+                    this.richTextBox1.AppendText(Environment.NewLine);
+                }
+            }
+            else if (e.ClickedItem.Name == "tGBK")
+            {
+                tUTF8.Checked = false;
+                tANSI.Checked = false;
+                tGBK.Checked = true;
+                foreach (var item in File.ReadLines(fileName, Encoding.GetEncoding("GBK")))
+                {
+                    string tmp = item.Replace("\0", "");
+                    this.richTextBox1.AppendText(tmp);
+                    this.richTextBox1.AppendText(Environment.NewLine);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (File.Exists(Path.Combine(Application.StartupPath, "~tmp.txt")))
+            {
+                File.Delete(Path.Combine(Application.StartupPath, "~tmp.txt"));
+            }
+        }
+    }
+}
